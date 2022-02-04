2e3b04a055f780f2eaf1f088bf65bf1a002b0f7c
Matt Rude <matt@mattrude.com>
2022-02-04 01:43:49 -0600

    who know anymore

--- src/LoRaKeys.h
+++ src/LoRaKeys.h
@@ -3,29 +3,19 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <lmic.h>
-#include <hal/hal.h>
-#include <SPI.h>
-
-byte DEVADD[4];
-byte APPEUI[8];
-byte DEVEUI[8];
-byte APPKEY[16];
-byte NWKKEY[16];
-byte eepDEVADD[4];
-byte eepAPPEUI[8];
-byte eepDEVEUI[8];
-byte eepAPPKEY[16];
-byte eepNWKKEY[16];
 
 class LoRaKeys {
   private:
-    byte _deviceaddress;
+    byte eepDEVADD[4];
+    byte eepAPPEUI[8];
+    byte eepDEVEUI[8];
+    byte eepAPPKEY[16];
+    byte eepNWKKEY[16];
     int buildLoRaKeysABP(void);
     int buildLoRaKeysOTTA(void);
     int readEEPROM(unsigned int eeaddress );
   public:
-    LoRaKeys(byte);
+    LoRaKeys(void);
     void loadLoRaKeys(void);
 };
 
