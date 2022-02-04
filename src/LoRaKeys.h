4aadfe2003236b4b47de9f12da5ef6f471b28ace
Matt Rude <matt@mattrude.com>
2022-02-03 23:46:10 -0600

    Updates

--- src/LoRaKeys.h
+++ src/LoRaKeys.h
@@ -7,24 +7,25 @@
 #include <hal/hal.h>
 #include <SPI.h>
 
-class LoRaKeys
-{
+byte DEVADD[4];
+byte APPEUI[8];
+byte DEVEUI[8];
+byte APPKEY[16];
+byte NWKKEY[16];
+byte eepDEVADD[4];
+byte eepAPPEUI[8];
+byte eepDEVEUI[8];
+byte eepAPPKEY[16];
+byte eepNWKKEY[16];
+
+class LoRaKeys {
   public:
+    int _key_type;
+    LoRaKeys(uint8_t, uint8_t);
     void buildLoRaKeysABP(void);
     void buildLoRaKeysOTTA(void);
 
   private:
-    byte DEVADD[4];
-    byte APPEUI[8];
-    byte DEVEUI[8];
-    byte APPKEY[16];
-    byte NWKKEY[16];
-    byte eepDEVADD[4];
-    byte eepAPPEUI[8];
-    byte eepDEVEUI[8];
-    byte eepAPPKEY[16];
-    byte eepNWKKEY[16];
-
     void loadLoRaKeys(void);
     void readEEPROM(unsigned int eeaddress );
 };
